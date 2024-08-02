0a4b8ac3690b3511eb7814046f139722e4939ff0
Tahir Khan <tahir.khan@contour-software.com>
2024-08-02 17:29:02 +0500

    useAuths

--- src/pages/mainLanding/LandingPage.jsx
+++ src/pages/mainLanding/LandingPage.jsx
@@ -3,7 +3,7 @@ import { GoBroadcast } from "react-icons/go";
 import { Link, useNavigate } from "react-router-dom";
 import MainNavbar from "../../components/Navbar/MainNavbar";
 import ActiveEvents from "../activeEvents/ActiveEvents";
-import useAuth from "../../components/UseAuth";
+import useAuth from "../useAuth/UseAuth";
 
 const LandingPage = () => {
   const navigate = useNavigate();
